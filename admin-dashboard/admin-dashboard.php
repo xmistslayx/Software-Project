9c3eaa7b6c2173f4c66e51cb96cb29fe8bfd5af7
A-abd <65906361+A-abd@users.noreply.github.com>
2025-03-04 01:39:41 +0000

    added basic html to the dashboards and index

--- admin-dashboard/admin-dashboard.php
+++ admin-dashboard/admin-dashboard.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html>
+<a href="room-management.php">
+    <button>Rooms Management</button>
+</a>
+<a href="guest-management.php">
+    <button>Guest Management</button>
+</a>
+<a href="../index.php">
+    <button>Back</button>
+</a>
+</html>
--- guest-dashboard/guest-dashboard.php
+++ guest-dashboard/guest-dashboard.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html>
+<a href="payments-page.php">
+    <button>Payments Page</button>
+</a>
+<a href="profile-management.php">
+    <button>Profile Management</button>
+</a>
+<a href="services-management.php">
+    <button>Services Management</button>
+</a>
+<a href="../index.php">
+    <button>Back</button>
+</a>
+</html>
