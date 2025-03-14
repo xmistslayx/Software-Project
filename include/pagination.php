3217206b6c78d3ec9a0ed5f181ac64bb718161fd
A-abd <65906361+A-abd@users.noreply.github.com>
2025-03-14 11:43:04 +0000

    Removed next and previous buttons

--- include/pagination.php
+++ include/pagination.php
@@ -4,17 +4,9 @@ function generatePagination($currentPage, $totalPages, $url)
 {
     $pagination = '<div class="pagination">';
 
-    if ($currentPage > 1) {
-        $pagination .= '<a href="' . $url . '?page=' . ($currentPage - 1) . '" class="button">Previous</a>';
-    }
-
     for ($i = 1; $i <= $totalPages; $i++) {
         $activeClass = ($i == $currentPage) ? 'active' : '';
         $pagination .= '<a href="' . $url . '?page=' . $i . '" class="button ' . $activeClass . '">' . $i . '</a>';
-    }
-
-    if ($currentPage < $totalPages) {
-        $pagination .= '<a href="' . $url . '?page=' . ($currentPage + 1) . '" class="button">Next</a>';
     }
 
     $pagination .= '</div>';
